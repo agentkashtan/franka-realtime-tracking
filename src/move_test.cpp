152f6fd98de99bc235f068d65d354769433c6ff6
RoboHub User <robohub@labwork3.uwaterloo.ca>
2023-08-16 15:26:16 -0400

    modified many things

--- src/move_test.cpp
+++ src/move_test.cpp
@@ -14,6 +14,14 @@
 
 #include "examples_common.h"
 
+#include <librealsense2/rs.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+using namespace std;
+
 /**
  * @example cartesian_impedance_control.cpp
  * An example showing a simple cartesian impedance controller without inertia shaping
@@ -23,12 +31,129 @@
  * @warning collision thresholds are set to high values. Make sure you have the user stop at hand!
  */
 
+mutex pose_mutex;
+bool pose_initialized = false;
+float markerLength = 0.06;
+cv::Mat cameraMatrix(3,3, CV_32FC1), distCoeffs;
+Eigen::Quaterniond orientation_d;
+Eigen::Vector3d position_d;
+Eigen::Vector3d position_p;
+
+cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_5X5_50);
+//cv::aruco::Dictionary dictionary = cv::aruco::extendDictionary(1, 5);
+Eigen::Isometry3d pose_offset;
+Eigen::Vector3d t_offset(0, 0, 0.3);
+Eigen::Isometry3d g_T_t;
+
+void realsense_callback(const rs2::frame& frame) {
+    Eigen::Isometry3d g_T_c;
+    g_T_c.setIdentity();
+    g_T_c.linear() = Eigen::AngleAxisd(-1.57, Eigen::Vector3d::UnitY()).toRotationMatrix();
+    g_T_c.translation() << 0., 0., 0.06;
+	
+    rs2::frameset fs = frame.as<rs2::frameset>();
+    if(!fs)return;
+    rs2::video_frame cur_frame = fs.get_color_frame();
+    if(!cur_frame)return;
+
+    // Convert the RealSense frame to a cv::Mat
+    rs2::video_frame video_frame = cur_frame.as<rs2::video_frame>();
+    int width = video_frame.get_width();
+    int height = video_frame.get_height();
+    int channels = video_frame.get_bytes_per_pixel();
+    int stride = video_frame.get_stride_in_bytes();
+
+    // The image data is stored in a shared pointer
+    const void* frame_data = video_frame.get_data();
+    try {
+    cv::Ptr<cv::aruco::DetectorParameters> detectorParams(new cv::aruco::DetectorParameters);
+    
+    cv::Mat image(height, width, CV_8UC(channels), const_cast<void*>(frame_data), stride);
+    vector<int> ids;
+    vector<vector<cv::Point2f>> corners, rejected;     
+    cv::aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
+    
+    cv::Mat objPoints(4, 1, CV_32FC3);
+    objPoints.ptr<cv::Vec3f>(0)[0] = cv::Vec3f(-markerLength/2.f, markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[1] = cv::Vec3f(markerLength/2.f, markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(markerLength/2.f, -markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
+
+    //cv::Mat outputImage;
+    //image.copyTo(outputImage);    
+    cout << "[CB] id " << ids.size() << endl;
+    if (ids.size() == 1 && ids.front() == 0) {
+    	cv::Vec3d rvec, tvec;
+	vector<int> dist;
+	cv::solvePnP(objPoints, corners.at(0), cameraMatrix, dist, rvec, tvec);
+
+	cv::drawFrameAxes(image, cameraMatrix, dist, rvec, tvec, 0.1);	
+	
+	// construct 4x4 transformation matrix
+
+	// rotation
+	cv::Mat rot_mat(3, 3, CV_32FC1); ;
+	cv::Rodrigues(rvec, rot_mat);
+	Eigen::Matrix3d eigen_matrix;
+	cv::cv2eigen(rot_mat, eigen_matrix);
+	Eigen::Isometry3d camera_T_tag;
+	camera_T_tag.linear() = eigen_matrix;
+	Eigen::Vector3d eigen_translation;
+	cv::cv2eigen(tvec,eigen_translation);
+	camera_T_tag.translation() = eigen_translation;
+
+	// Should work as follows
+	
+	g_T_t = g_T_c * camera_T_tag;	
+	position_p = g_T_t.translation();
+	//cout << "pos in t: " << tvec[0] << " " << tvec[1] << " " << tvec[2] << endl << flush;
+	cout << "pos in g: " <<position_p[0] << " " << position_p[1] << " " << position_p[2] << endl << endl << flush;
+
+	//// TODO: run aruco tag detector in opencv
+	//// TODO: compute tag pose (using instrinsics)
+	//// assign pose to global variable
+	//lock_guard<std::mutex> lock(pose_mutex);
+	//pose_initialized = true;
+    }
+    cv::imshow("tasde", image);
+    cv::waitKey(5);
+    } catch (std::exception const & ex ) {
+    	std::cout << ex.what() << std::endl;
+    }
+};	
+
+
 int main(int argc, char** argv) {
   // Check whether the required arguments were passed
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
+    cerr << "Usage: " << argv[0] << " <robot-hostname>" << endl;
     return -1;
   }
+  cv::Mat markerImage;
+  cv::aruco::drawMarker(dictionary, 0, 200, markerImage, 1);
+  cv::imwrite("marker.png", markerImage);
+
+  // Define Transformation from camera to gripper frame
+  // g_T_c.linear() = Eigen::AngleAxisd(-1.57, Eigen::Vector3d::UnitY()).toRotationMatrix();
+  // g_T_c.translation() << 0., 0., 0.06;
+  // g_T_c.translation() << 0.06, 0., 0.;
+  
+  // create librealsense pipelinei
+  
+  rs2::pipeline pipe;
+  rs2::config cfg;
+  const int fps = 6;
+  cfg.enable_stream(RS2_STREAM_COLOR, 1920, 1080, RS2_FORMAT_RGB8, fps);
+  cfg.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_Z16, fps);
+  cfg.enable_device("819612070440");
+  pipe.start(cfg,realsense_callback);
+  auto intrinsics = pipe.get_active_profile().get_stream(rs2_stream::RS2_STREAM_COLOR).as<rs2::video_stream_profile>().get_intrinsics();
+
+   //TODO: convert instrinsics to OpenCV
+   float camera_data[3][3] = {{intrinsics.fx, 0, intrinsics.width / 2.f}, {0, intrinsics.fy, intrinsics.height / 2.f}, {0, 0, 1}};
+   cameraMatrix = cv::Mat(3, 3, CV_32FC1, &camera_data);
+   
+  // save to global variable
 
   // Compliance parameters
   const double translational_stiffness{150.0};
@@ -53,28 +178,10 @@ int main(int argc, char** argv) {
     franka::RobotState initial_state = robot.readOnce();
 
     // equilibrium point is the initial position
-    Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
-    Eigen::Vector3d position_d(initial_transform.translation());
-    //position_def[0] = 0.3;
-    //position_def[1] = 0.3;
-    //position_def[2] = 0.5;
-    Eigen::Matrix3d rot;
+    Eigen::Isometry3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
+    position_d = Eigen::Vector3d(initial_transform.translation());
+    orientation_d = Eigen::Quaterniond(initial_transform.rotation());
     
-    rot.setIdentity();
-
-    Eigen::VectorXd x {{0.0, 1.0, 0.0}};
-    Eigen::VectorXd y {{0.0, 0.0, 1.0}};
-    Eigen::VectorXd z {{1.0, 0.0, 0.0}};
-
-    rot.col(0) = x;
-    rot.col(1) = y;
-    rot.col(2) = z;
-    
-    Eigen::Vector3d zero_v;
-    double total_time = 0;
-    //rot = Eigen::Matrix3d(initial_transform.rotation());
-    Eigen::Quaterniond orientation_d(rot);
-
     // set collision behavior
     robot.setCollisionBehavior({{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
                                {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
@@ -82,14 +189,12 @@ int main(int argc, char** argv) {
                                {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}});
 
     // define callback for the torque control loop
-    std::function<franka::Torques(const franka::RobotState&, franka::Duration)>
+    function<franka::Torques(const franka::RobotState&, franka::Duration)>
         impedance_control_callback = [&](const franka::RobotState& robot_state,
                                          franka::Duration time) -> franka::Torques {
       // get state variables
-      total_time += time.toMSec();
-      std::cout << total_time / 1000 << std::endl;
-      std::array<double, 7> coriolis_array = model.coriolis(robot_state);
-      std::array<double, 42> jacobian_array =
+      array<double, 7> coriolis_array = model.coriolis(robot_state);
+      array<double, 42> jacobian_array =
           model.zeroJacobian(franka::Frame::kEndEffector, robot_state);
 
       // convert to Eigen
@@ -97,15 +202,20 @@ int main(int argc, char** argv) {
       Eigen::Map<const Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
       Eigen::Map<const Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
       Eigen::Map<const Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
-      Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
+      Eigen::Isometry3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
       Eigen::Vector3d position(transform.translation());
       Eigen::Quaterniond orientation(transform.rotation());
+
+      Eigen::Isometry3d O_T_EE(transform);
+
+      Eigen::Isometry3d O_T_tag = O_T_EE * g_T_t;
+      Eigen::Vector3d den(O_T_tag.translation());
+      //cout << "tag in  base: "<< den[0] << " " << den[1] << " " << den[2] << endl << endl << flush; 
 
       // compute error to desired equilibrium pose
       // position error
       Eigen::Matrix<double, 6, 1> error;
       error.head(3) << position - position_d;
-
       // orientation error
       // "difference" quaternion
       if (orientation_d.coeffs().dot(orientation.coeffs()) < 0.0) {
@@ -124,25 +234,24 @@ int main(int argc, char** argv) {
       tau_task << jacobian.transpose() * (-stiffness * error - damping * (jacobian * dq));
       tau_d << tau_task + coriolis;
 
-      std::array<double, 7> tau_d_array{};
+      array<double, 7> tau_d_array{};
       Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_d;
 
-      std::cout << error.tail(3).norm() << std::endl;
-      //std::cout << std::endl;
+      //cout << error.tail(3).norm() << endl;
       return tau_d_array;  
     };
-
+    
     // start real-time control loop
-    std::cout << "WARNING: Collision thresholds are set to high values. "
-              << "Make sure you have the user stop at hand!" << std::endl
-              << "After starting try to push the robot and see how it reacts." << std::endl
-              << "Press Enter to continue..." << std::endl;
-    std::cin.ignore();
+    cout << "WARNING: Collision thresholds are set to high values. "
+              << "Make sure you have the user stop at hand!" << endl
+              << "After starting try to push the robot and see how it reacts." << endl
+              << "Press Enter to continue..." << endl;
+    cin.ignore();
     robot.control(impedance_control_callback);
 
   } catch (const franka::Exception& ex) {
     // print exception
-    std::cout << ex.what() << std::endl;
+    cout << ex.what() << endl;
   }
 
   return 0;
