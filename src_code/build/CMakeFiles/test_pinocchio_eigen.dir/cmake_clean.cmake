52c80949ca6c87c9d1ff79d2da39afd2dbe04e69
oleksii <ozamozhs@uwaterloo.ca>
2024-10-02 20:44:45 -0400

    init

---
+++ src_code/build/CMakeFiles/test_pinocchio_eigen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_pinocchio_eigen.dir/pin_test.cpp.o"
+  "CMakeFiles/test_pinocchio_eigen.dir/pin_test.cpp.o.d"
+  "test_pinocchio_eigen"
+  "test_pinocchio_eigen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_pinocchio_eigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
